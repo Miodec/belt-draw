23be92f3864b4189c9011a2575a3642e1580a248
Miodec <jack@monkeytype.com>
2026-02-06 16:20:39 +0100

    fix cursor clearing if the player disabled smart belt building

---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+  {
+    type = "bool-setting",
+    name = "belt-draw-smart-belt-building-disabled",
+    setting_type = "runtime-per-user",
+    default_value = false,
+    order = "a",
+  }
+})
--- src/events/cancel.lua
+++ src/events/cancel.lua
@@ -8,16 +8,18 @@ script.on_event("belt-draw-cancel", function(event)
 
   cleanup(player, false)
 
-  -- if player disabled smart belt building, this will fix cursor clear while drawing
-  -- local cursor_stack = player.cursor_stack
-  -- if cursor_stack then
-  --   if not cursor_stack.valid_for_read or is_bd_tool(cursor_stack.name) or player.clear_cursor() then
-  --     local tier_info = tiers[storage.current_tier]
+  if settings.get_player_settings(player)["belt-draw-smart-belt-building-disabled"].value then
+    -- if player disabled smart belt building, this will fix cursor clear while
+    local cursor_stack = player.cursor_stack
+    if cursor_stack then
+      if not cursor_stack.valid_for_read or is_bd_tool(cursor_stack.name) or player.clear_cursor() then
+        local tier_info = tiers[storage.current_tier]
 
-  --     storage.current_tier = tier_info.name
-  --     cursor_stack.set_stack({ name = tier_info.preview_tool })
-  --   end
-  -- end
-
-  player.clear_cursor()
+        storage.current_tier = tier_info.name
+        cursor_stack.set_stack({ name = tier_info.preview_tool })
+      end
+    end
+  else
+    player.clear_cursor()
+  end
 end)
